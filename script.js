c8625dba2f9ab0afd466da2da8d375e9447c22b6
Lunairefine <teamcyber2809@gmail.com>
2023-08-14 08:48:30 +0700

    Maintenance Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h4Element = document.getElementById('typeWriter');
+const h4Element = document.getElementById('autoType');
 const text = h4Element.textContent;
 
 let charIndex = 0;
@@ -7,7 +7,7 @@ function typeWriter() {
   if (charIndex < text.length) {
     h4Element.textContent = text.slice(0, charIndex + 1);
     charIndex++;
-    setTimeout(typeWriter, 100); // Jeda waktu antara setiap karakter (ms)
+    setTimeout(typeWriter, 90); // Jeda waktu antara setiap karakter (ms)
   }
 }
 
